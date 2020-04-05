03fd07a4e9bef751c7de0d3d236b53eee7d190be
Aidan Grabe <aidan@teamwork.com>
2020-04-05 16:37:01 +0100

    Add event emitter to entities

--- src/entity.js
+++ src/entity.js
@@ -11,10 +11,20 @@ class Entity {
 
     constructor() {
         this._active = true;
+        this._emitter = new EventEmitter();
         this.sprite = null;
     }
 
+    on(event, listener) {
+        this._emitter.on(event, listener);
+    }
+
+    removeEventListener(event, listener) {
+        this._emitter.removeListener(event, listener);
+    }
+
     onAddedToStage(stage) {
+        this.emitEvent('stage.add');
     }
 
     onActiveChanged(active) {
@@ -25,12 +35,18 @@ class Entity {
     }
 
     onRemovedFromStage(stage) {
+        this.emitEvent('stage.remove');
     }
 
     onKeyPressed(key) {
     }
 
+    emitEvent(event, payload) {
+        this._emitter.emit(event, payload);
+    }
+
     kill() {
+        this.emitEvent('kill');
         this.active = false;
     }
 
---
+++ src/event_emitter.js
@@ -0,0 +1,30 @@
+class EventEmitter {
+
+    constructor() {
+        this.listeners = {};
+    }
+
+    emit(event, payload) {
+        const listeners = this.listeners[event] || [];
+
+        for (let listener of listeners) {
+            listener(payload);
+        }
+    }
+
+    on(event, listener) {
+        const list = this.listeners[event] || [];
+        list.push(listener);
+        this.listeners[event] = list;
+    }
+
+    removeListener(event, listener) {
+        const list = this.listeners[event] || [];
+        const position = list.indexOf(listener);
+
+        if (position != -1) {
+            list.splice(position, 1);
+        }
+    }
+
+}
